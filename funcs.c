6a952fb755f22ec75f6743c5f70d58c5798f4fec
AgustinGallotti <Agustingallotti23@gmail.com>
2022-05-12 16:15:21 -0700

    nop

--- funcs.c
+++ funcs.c
@@ -126,5 +126,6 @@ stack_t *swap(stack_t **stack,
 stack_t *nop(stack_t **stack __attribute__ ((unused)),
 	  unsigned int line_number __attribute__((unused)))
 {
-	return (0);
+	(void)stack;
+	(void)line_number;
 }
